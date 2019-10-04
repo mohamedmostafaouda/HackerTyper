161647a4529fef5e73f522ff935f05ae9479a320
mohamedmostafaouda <55032769+mohamedmostafaouda@users.noreply.github.com>
2019-10-04 21:24:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,16 +4,31 @@ $(function () {
     let i=0; 
     $('.cursor2').hide();
     
-    $(document).keypress( function () {
+    $(document).keydown( function () {
+        var key = event.keyCode || event.charCode;
+
+        if( key == 8 ){
+            i--;
+            while (expression[i]==' ')i-=1;
+            while (expression[i]=='<')i-=3;
+            while (expression[i]=='&')i-=3;
+            $('#hacker').html(expression.substr(0,i));
+        }
+        else{
         $('.cursor').hide();
         $('.cursor2').show();
         while (expression[i]==' ')i+=1;
+        while (expression[i]=='<')i+=3;
+        while (expression[i]=='&')i+=3;
+        var c = document.querySelector("#scroll");
+        c.scrollIntoView();
         $('#hacker').html(expression.substr(0,i));
         i++;
         setTimeout(() => {
             $('.cursor').show();
             $('.cursor2').hide();
         }, 2000);
+    }
     });
     
 })
